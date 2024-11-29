3ebb439d5c05046d6d5d0a5ab10b2672a95adb48
petickovit <160601349+petickovit@users.noreply.github.com>
2024-11-29 09:46:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao (
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação.'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital da França?',
+    'A Capital da França é Paris.'
+)
+
+criaCartao (
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa.'
+)
+
+criaCartao (
+    'Língua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em Inglês se diz HI (RAI)'
+)
